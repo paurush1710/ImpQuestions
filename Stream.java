3eab384f69a88289177746d8a597d2e364cdac42
Paurush <40339719+paurush1710@users.noreply.github.com>
2023-07-23 08:23:31 +0530

    Create Stream.java

---
+++ Stream.java
@@ -0,0 +1,14 @@
+	HashMap<String, Integer> hm = new HashMap<String, Integer>();
+ 
+        // enter data into hashmap
+        hm.put("Math", 98);
+        hm.put("Data Structure", 85);
+        hm.put("Database", 91);
+        hm.put("Java", 95);
+        hm.put("Operating System", 79);
+        hm.put("Networking", 80);
+        Map<String, Integer> hm1 = sortByValue(hm);
+
+------------------------------------------
+
+  
