c7aa28a388490583e4fb933e01761d78f395f86e
Paurush <40339719+paurush1710@users.noreply.github.com>
2025-04-06 18:38:48 +0530

    Create Switch Cases.java

---
+++ Programs/Switch Cases.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        
+        String fruit = "Apple";
+        
+        switch(fruit)
+        {
+            case "Mango":
+                System.out.println("This is Mango");
+                break;
+                
+            case "Apple":
+                System.out.println("This is Apple");
+                break;
+                
+            default:
+                System.out.println("Other Fruit");
+        }
+    }
+}
