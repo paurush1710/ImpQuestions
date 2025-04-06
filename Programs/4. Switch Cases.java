1c23d889b2eea5541cdd95765c76d827fe3e5f29
Paurush <40339719+paurush1710@users.noreply.github.com>
2025-04-06 19:25:47 +0530

    Update 4. Switch Cases.java

--- Programs/4. Switch Cases.java
+++ Programs/4. Switch Cases.java
@@ -24,6 +24,64 @@ class Main {
             case "Mango" -> System.out.println("This is Mango");
             default -> System.out.println("Other Fruit");
         }
+
+        int i = 5;
+        switch(i)
+        {
+            case 1,2,3,4,5 -> System.out.println("Weekday");
+            case 6,7 -> System.out.println("Weekend");
+        }
+
+        // nested Switch
+
+        int id = 3;
+        String department = "IT";
+
+        switch(id){
+            case 1:
+                 System.out.println("Paurush Sharma");
+                 break;
+                
+            case 2:
+                 System.out.println("Ram Sharma");
+                 break;
+                 
+            case 3:
+                 System.out.println("Shyam Sharma");
+                 switch(department)
+                 {
+                     case "IT":
+                         System.out.println("Info Tech dept");
+                         break;
+                         
+                    case "Mech":
+                        System.out.println("Mechanical dept");
+                        break;
+                        
+                    default:
+                        System.out.println("No dept");
+                 }
+                 break;
+                 
+            default:
+                 System.out.println("No Employee");
+        }
+
+        // enhanced nested switch
+
+        switch(id){
+            case 1 -> System.out.println("Paurush Sharma");
+            case 2 -> System.out.println("Ram Sharma");
+            case 3 -> {
+                System.out.println("Shyam Sharma");
+                switch(department){
+                    case "IT"-> System.out.println("Info Tech dept");
+                    case "Mech" -> System.out.println("Mechanical dept");
+                    default -> System.out.println("No dept");
+                }
+            }
+            default -> System.out.println("No emp");
+        }
         
     }
 }
